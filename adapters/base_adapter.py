66505ddfc3beaed1beff6e0f79f364332aa5c24f
Ethan <ethan.harriz@gmail.com>
2025-05-04 04:47:56 -0400

    Add base music program adapter

---
+++ adapters/base_adapter.py
@@ -0,0 +1,19 @@
+from abc import abstractmethod
+from io import BytesIO
+from typing import Optional
+
+from util import Config
+
+
+class BaseAdapter:
+    def __init__(self, adapter_name: str):
+        self._config = Config()
+        self._settings = self._config.get_section(adapter_name)
+        self.adapter_name = adapter_name
+
+    def _save_config(self, data) -> None:
+        self._config.save_section(self.adapter_name, data)
+
+    @abstractmethod
+    async def get_current_song(self, session) -> tuple[str, list, Optional[BytesIO]]:
+        pass
